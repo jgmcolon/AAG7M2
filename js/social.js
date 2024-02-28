2c583d311afc02d9b527d0d143563560ffbe1d40
Manases Lovera <manaseslovera07@gmail.com>
2024-02-28 09:59:44 -0400

    social button

---
+++ js/social.js
@@ -0,0 +1,13 @@
+let btnFacebook = getElementById('icon-facebook');
+let btnTwitter = getElementById('icon-twitter');
+let btnSkype = getElementById('icon-skype');
+let btnInstagram = getElementById('icon-instagram');
+
+btnFacebook.addEventListener('click', button);
+btnTwitter.addEventListener('click', button);
+btnSkype.addEventListener('click', button);
+btnInstagram.addEventListener('click', button);
+
+function button(){
+    alert("Aun no tenemos las redes sociales bro...");
+}
